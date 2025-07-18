b57310aefd9dcac70cd6f79c5cc348e6b10264f8
LABHESHNEVREKAR <labheshoffical@gmail.com>
2025-07-18 20:28:21 +0530

    fix: install Python 3.11 explicitly using build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+echo "Installing Python 3.11..."
+apt-get update
+apt-get install -y python3.11 python3.11-venv python3.11-dev
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
+python3 --version
